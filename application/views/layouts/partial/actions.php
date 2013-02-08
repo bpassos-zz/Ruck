abe5023f574df364bcf694ee63ff7ac16897e2b5
Matthew Pennell <github@matthewpennell.com>
2013-02-08 15:23:08 +0000

    Add home button.

--- application/views/layouts/partial/actions.php
+++ application/views/layouts/partial/actions.php
@@ -1,4 +1,6 @@
 <div class="actions">
+	
+	<a href="/gtd/"><button style="font-size: 24px; position: relative; top: 3px;">&#8962;</button></a>
 
 	<a href="/gtd/projects/create"><button>New Project</button></a>
 
--- application/views/tasks/detail.php
+++ application/views/tasks/detail.php
@@ -1,3 +1,5 @@
 <h1><?php echo $task['description']; ?></h1>
 
 <p><?php echo $task['notes']; ?></p>
+
+<p><a href="/gtd/tasks/delete/<?php echo $task['id']; ?>">Delete this task</a></p>
