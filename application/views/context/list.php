bd169ef23a6b7f6df78c610e70e8da1291a5b085
Matthew Pennell <github@matthewpennell.com>
2013-02-09 14:39:33 +0000

    Add link to view all tasks within a specific Context.

--- application/views/context/list.php
+++ application/views/context/list.php
@@ -2,8 +2,11 @@
 
 <p><?php echo $context['description']; ?></p>
 
-<ul>
+<ul class="tasks">
 	<?php foreach ($tasks as $task): ?>
-		<li><a href="/gtd/tasks/detail/<?php echo $task->id; ?>"><?php echo $task->description; ?></a> <a href="/gtd/tasks/delete/<?php echo $task->id; ?>" class="mini">Delete</a></li>
+		<li data-context-id="<?php echo $task->context_id; ?>">
+			<a href="/gtd/tasks/detail/<?php echo $task->id; ?>"><?php echo $task->description; ?></a>
+			<a href="/gtd/tasks/delete/<?php echo $task->id; ?>" class="mini">Delete</a>
+		</li>
 	<?php endforeach; ?>
 </ul>
--- application/views/layouts/partial/contexts.php
+++ application/views/layouts/partial/contexts.php
@@ -1,6 +1,11 @@
 <ul id="contexts">
 	<?php foreach ($context_list as $context): ?>
-		<li data-context-id="<?php echo $context->id; ?>"><a href="#" class="context"><?php echo $context->name; ?></a></li>
+		<li data-context-id="<?php echo $context->id; ?>">
+			<a href="#" class="context"><?php echo $context->name; ?></a>
+			<div>
+				<a href="/gtd/contexts/<?php echo $context->id; ?>">Find all</a>
+			</div>
+		</li>
 	<?php endforeach; ?>
 	<li><a href="/gtd/contexts/create">+New</a></li>
 </ul>
