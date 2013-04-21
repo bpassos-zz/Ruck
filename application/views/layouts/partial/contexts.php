9386a0af2725a2d72adf6e91ab089fa5dc8c42b0
Matthew Pennell <github@matthewpennell.com>
2013-04-21 20:04:21 +0100

    Change indication of selected context.

--- application/views/layouts/partial/contexts.php
+++ application/views/layouts/partial/contexts.php
@@ -2,7 +2,7 @@
 <ul>
 	<?php foreach ($context_list as $context): ?>
 		<li data-context-id="<?php echo $context->id; ?>">
-			<a href="#" class="context pill"><?php echo $context->name; ?></a>
+			<a href="#" class="context"><?php echo $context->name; ?></a>
 		</li>
 	<?php endforeach; ?>
 </ul>
