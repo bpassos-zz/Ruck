a8dfc0818e60d1bb1d470aa45c8d5a9003c041b7
Matthew Pennell <github@matthewpennell.com>
2013-05-11 14:41:11 +0100

    Making minor changes to layout.

--- application/views/project/someday-maybe.php
+++ application/views/project/someday-maybe.php
@@ -4,6 +4,6 @@
 
 <ul class="tasks">
 	<?php foreach ($projects as $project): ?>
-		<li><a href="/gtd/projects/<?php echo $project->id; ?>"><?php echo $project->name; ?></a></li>
+		<li><a href="/gtd/projects/<?php echo $project->id; ?>" class="task"><?php echo $project->name; ?></a></li>
 	<?php endforeach; ?>
 </ul>
