cdfbed7598e090cfdf4e03405f03158b4678a9af
Matthew Pennell <github@matthewpennell.com>
2013-05-11 17:09:07 +0100

    Fix display of upcoming tasks in Calendar view.

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -2,16 +2,21 @@
 
 <?php if (count($upcoming) > 0): ?>
 	
-	<?php foreach ($upcoming as $task): ?>
-		<div class="calendar-item">
-			<a href="/gtd/tasks/detail/<?php echo $task->id; ?>">
-				<strong class="date"><?php echo date('M j', strtotime($task->due)); ?></strong>
-				<br>
-				<?php echo character_limiter($task->description, 20); ?>
-			</a>
-			<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="pill project calendar-project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
-		</div>
-	<?php endforeach; ?>
+	<ul class="tasks">
+		<?php foreach ($upcoming as $task): ?>
+			<li data-context-id="<?php echo $task->context_id; ?>">
+				<a href="/gtd/tasks/delete/<?php echo $task->id; ?>/home" class="delete">&#10004;</a>
+				<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="task"><?php echo $task->description; ?></a>
+				<?php if ($task->due): ?>
+					<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="pill <?php echo (strtotime($task->due) + 86400 > time()) ? 'date' : 'overdue'; ?>"><?php echo date('F j', strtotime($task->due)); ?></a>
+					<?php if ($task->recurs): ?>
+						<img src="/i/recurs.png" alt="Recurs">
+					<?php endif; ?>
+				<?php endif; ?>
+				<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
 
 <?php else: ?>
 
