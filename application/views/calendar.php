811b4f1b1b939b933fca199038482dfc6d9b3e15
Matthew Pennell <github@matthewpennell.com>
2013-05-14 21:21:02 +0100

    Redesign the date block on task lists.

--- application/views/calendar.php
+++ application/views/calendar.php
@@ -1,17 +1,41 @@
+<?php if (count($overdue) > 0): ?>
+	
+	<h1>Overdue Tasks</h1>
+	
+	<ul class="tasks dates">
+		<?php foreach ($overdue as $task): ?>
+			<li data-context-id="<?php echo $task->context_id; ?>">
+				<span class="date overdue">
+					<strong><?php echo date("j", strtotime($task->due)); ?></strong>
+					<?php echo date("M", strtotime($task->due)); ?>
+				</span>
+				<a href="/gtd/tasks/delete/<?php echo $task->id; ?>/home" class="delete">&#10004;</a>
+				<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="task"><?php echo $task->description; ?></a>
+				<?php if ($task->recurs): ?>
+					<img src="/i/recurs.png" alt="Recurs">
+				<?php endif; ?>
+				<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+
+<?php endif; ?>
+
 <h1>Due Today</h1>
 
 <?php if (count($today) > 0): ?>
 	
-	<ul class="tasks">
+	<ul class="tasks dates">
 		<?php foreach ($today as $task): ?>
 			<li data-context-id="<?php echo $task->context_id; ?>">
+				<span class="date today">
+					<strong><?php echo date("j", strtotime($task->due)); ?></strong>
+					<?php echo date("M", strtotime($task->due)); ?>
+				</span>
 				<a href="/gtd/tasks/delete/<?php echo $task->id; ?>/home" class="delete">&#10004;</a>
 				<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="task"><?php echo $task->description; ?></a>
-				<?php if ($task->due): ?>
-					<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="pill <?php echo (strtotime($task->due) + 86400 > time()) ? 'date' : 'overdue'; ?>"><?php echo date('F j', strtotime($task->due)); ?></a>
-					<?php if ($task->recurs): ?>
-						<img src="/i/recurs.png" alt="Recurs">
-					<?php endif; ?>
+				<?php if ($task->recurs): ?>
+					<img src="/i/recurs.png" alt="Recurs">
 				<?php endif; ?>
 				<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
 			</li>
@@ -28,38 +52,17 @@
 	
 	<h1>Due Tomorrow</h1>
 	
-	<ul class="tasks">
+	<ul class="tasks dates">
 		<?php foreach ($tomorrow as $task): ?>
 			<li data-context-id="<?php echo $task->context_id; ?>">
+				<span class="date tomorrow">
+					<strong><?php echo date("j", strtotime($task->due)); ?></strong>
+					<?php echo date("M", strtotime($task->due)); ?>
+				</span>
 				<a href="/gtd/tasks/delete/<?php echo $task->id; ?>/home" class="delete">&#10004;</a>
 				<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="task"><?php echo $task->description; ?></a>
-				<?php if ($task->due): ?>
-					<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="pill <?php echo (strtotime($task->due) + 86400 > time()) ? 'date' : 'overdue'; ?>"><?php echo date('F j', strtotime($task->due)); ?></a>
-					<?php if ($task->recurs): ?>
-						<img src="/i/recurs.png" alt="Recurs">
-					<?php endif; ?>
-				<?php endif; ?>
-				<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
-
-<?php endif; ?>
-
-<?php if (count($overdue) > 0): ?>
-	
-	<h1>Overdue Tasks</h1>
-	
-	<ul class="tasks">
-		<?php foreach ($overdue as $task): ?>
-			<li data-context-id="<?php echo $task->context_id; ?>">
-				<a href="/gtd/tasks/delete/<?php echo $task->id; ?>/home" class="delete">&#10004;</a>
-				<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="task"><?php echo $task->description; ?></a>
-				<?php if ($task->due): ?>
-					<a href="/gtd/tasks/detail/<?php echo $task->id; ?>" class="pill <?php echo (strtotime($task->due) + 86400 > time()) ? 'date' : 'overdue'; ?>"><?php echo date('F j', strtotime($task->due)); ?></a>
-					<?php if ($task->recurs): ?>
-						<img src="/i/recurs.png" alt="Recurs">
-					<?php endif; ?>
+				<?php if ($task->recurs): ?>
+					<img src="/i/recurs.png" alt="Recurs">
 				<?php endif; ?>
 				<a href="/gtd/projects/<?php echo $task->project_id; ?>" class="project" title="<?php echo $task->project_name; ?>"><?php echo character_limiter($task->project_name, 20); ?></a>
 			</li>
